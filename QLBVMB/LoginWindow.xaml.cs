b508eaeccb91733569915bd2c49c86006401f7f8
DDP16 <phat16122004@gmail.com>
2023-12-07 22:27:24 +0700

    Add event button thoat - exit Login Window

--- QLBVMB/LoginWindow.xaml.cs
+++ QLBVMB/LoginWindow.xaml.cs
@@ -23,5 +23,10 @@ namespace QLBVMB
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
     }
 }
