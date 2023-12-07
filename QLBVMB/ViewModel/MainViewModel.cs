4ffdacd8ad6598ed6f25783153d99207837c38d1
highlamjay <caoduonglam61@gmail.com>
2023-12-07 20:30:57 +0700

    clip 4

---
+++ QLBVMB/ViewModel/MainViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace QLBVMB.ViewModel
+{
+    public class MainViewModel : BaseViewModel
+    {
+        public MainViewModel()
+        {
+            MessageBox.Show("Đã vào trong MainViewModel");
+        }
+
+    }
+}
